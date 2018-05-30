cfa89dba2bca3c2fdf8043dca736021055383d9e
Philip Zelazowski <phl_z@yahoo.co.uk>
2018-05-30 13:19:17 +0100

    stopped logging, added calorie counter in /project

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
-const express = require('express'); 
-
+const express = require('express');
 const hbs = require('hbs');      // hbs is an express.js view engine for handlebars.js
-
 const fs = require('fs');
 const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('capitalize', (text) => {
+    return text.toUpperCase();
+});
+
 app.set('view engine','hbs');  // a ('key','value') pair
 
 app.use( (request, response, next) => {
     var now = new Date().toString();
     var log = `${now}: ${request.method} ${request.url}`;
-    console.log(log); 
-    fs.appendFile('server.log', log + '\n', (err) => {
-        if(err) console.log('unable to update server.log');
-    });
+    console.log(log);
+    // fs.appendFile('server.log', log + '\n', (err) => {
+    //     if(err) console.log('unable to update server.log');
+    // });
     next();
 });
 
@@ -29,18 +35,10 @@ app.use( (request, response, next) => {
 /* setting up middleware defines how express is going to work. we're passing a function, 'static', an express built in m/w. This gives us a static directory where we can do html, css and do a web page */
 app.use(express.static(__dirname + '/public'));
 
-hbs.registerHelper('getCurrentYear', () => {
-    return new Date().getFullYear();
-});
-
-hbs.registerHelper('capitalize', (text) => {
-    return text.toUpperCase();
-});
-
  // handler for HTTP get request ..
 app.get('/', (request, response) => {
     response.render('root.hbs', {
-        welcome:'Hello Wankers!',
+        welcome:'Hello People!',
         pageTitle:'Home Page'
 //        currentYear: new Date().getFullYear()
     })
@@ -52,6 +50,13 @@ app.get('/about', (request, response) => {
         pageTitle:'About Page'
 //        currentYear: new Date().getFullYear()
     });
+});
+
+app.get('/project', (request, response) => {
+    response.render('project.hbs', {
+        welcome:'Welcome to the projects page',
+        pageTitle:'Projects Page'
+    })
 });
 
 app.get('/phil', (request, response) => {
@@ -69,7 +74,7 @@ app.get('/bad', (request, response) => {
         errormessage:'unable to handle request'
     });
 });
-    
+
 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
